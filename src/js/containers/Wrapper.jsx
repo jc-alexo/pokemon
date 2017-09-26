47924cf0de6a7fc812d0d9950fe55c143d9361f5
Alex Ofoedu <alexander@junglecreations.com>
2017-09-26 11:03:22 +0100

    CORS error

--- src/js/containers/Wrapper.jsx
+++ src/js/containers/Wrapper.jsx
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
-
 import Title from '../components/Title';
+import reqwest from 'reqwest'
+
 
 class Wrapper extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            altText: false,
-            loading: true
+            pokemon: "ice"            
         };
 
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        this.setState((nextState) => {
-            return {
-                altText: !nextState.altText
-            } 
+
+        reqwest({
+            url: 'https://pokeapi.co/api/v2/pokemon/charizard',
+            headers: {
+                "Content-Type":"application/json",
+                "Access-Control-Allow-Origin": true,
+                "Origin": "http://localhost:3000"
+            }
+        }).then((response)=>{
+            this.setState({
+                pokemon: response.name
+            });
         });
+
+        // this.setState((prevState) => {
+        //     return {
+        //         pokemon: !prevState.pokemon
+        //     } 
+        // });
     }
 
     render() {
-        const { altText } = this.state;
+        const { pokemon } = this.state;
 
-        const titleText = altText ? "I am alternative text" : "Hello World";
+        // const titleText = altText ? "I am alternative text" : "Hello World";
 
         return (
             <div>
-                <Title text={titleText} />
-                <Title text="Dexatron" />
-                <button onClick={this.handleClick}>Change State</button>
+                <Title text={pokemon} />
+                <Title text="Dexterous" />
+                <button onClick={this.handleClick}>Switch Ice/Fire</button>
             </div>
         );
     }
